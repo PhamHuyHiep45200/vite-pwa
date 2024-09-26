ba05f05ab20c6ee630ff74c24c468b0bc8695a25
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-26 10:32:02 +0700

    fix file

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AssetLinks from './pages/AssetLinks.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={App} /> {/* 👈 Renders at /app/ */}
-        <Route path="/.well-known/assetlinks.json" Component={AssetLinks as React.ComponentType} /> {/* 👈 Renders at /app/ */}
       </Routes>
 
     </BrowserRouter>
